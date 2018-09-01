32f0b94b05482eca1c317e383c608fa59025d06c
Gunnar Holwerda <gunnarholwerda@gmail.com>
2018-09-01 14:58:08 -0700

    Add countdown for question expiration

--- src/app/question-card/question-card.component.ts
+++ src/app/question-card/question-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output, AfterViewInit, OnChanges, SimpleChanges } from '@angular/core';
 import { IQuestionResponse, IChoiceResponse } from '../models/question';
 
 @Component({
@@ -6,19 +6,46 @@ import { IQuestionResponse, IChoiceResponse } from '../models/question';
   templateUrl: './question-card.component.html',
   styleUrls: ['./question-card.component.css']
 })
-export class QuestionCardComponent implements OnInit {
+export class QuestionCardComponent implements OnInit, OnChanges {
   @Input() question: IQuestionResponse;
   @Input() disableActions = false;
   @Output() start: EventEmitter<IQuestionResponse> = new EventEmitter();
   @Output() results: EventEmitter<IQuestionResponse> = new EventEmitter();
   @Output() choiceSelected: EventEmitter<IChoiceResponse> = new EventEmitter();
 
+  secondsRemaining: number;
+  countdown: any;
+
   constructor() { }
 
   ngOnInit() { }
 
+  ngOnChanges(changes: SimpleChanges) {
+    const { question } = changes;
+    if (question && (question.firstChange || question.previousValue.expired !== question.currentValue.expired)) {
+      this.startCountdown();
+    }
+  }
+
+  startCountdown(): void {
+    this.secondsRemaining = this.secondsToExpiration();
+    this.countdown = setInterval(() => {
+      this.secondsRemaining--;
+    }, 1000);
+    setTimeout(() => {
+      clearInterval(this.countdown);
+      this.countdown = undefined;
+    }, this.secondsRemaining * 1000);
+  }
+
   onChoiceSelect(choice: IChoiceResponse): void {
     this.choiceSelected.emit(choice);
+  }
+
+  secondsToExpiration(): number {
+    const expirationDate = new Date(this.question.expired);
+    const currentTime = new Date();
+    return Math.floor(Math.max(0, (expirationDate.getTime() - currentTime.getTime()) / 1000));
   }
 
   onResultsCalculate(): void {
@@ -27,6 +54,10 @@ export class QuestionCardComponent implements OnInit {
 
   onSend(): void {
     this.start.emit(this.question);
+  }
+
+  expired(): boolean {
+    return this.secondsRemaining <= 0 || this.countdown === undefined;
   }
 
 }

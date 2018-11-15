182bcce83623e7502b09925d45e3f811fc0084fc
Gunnar Holwerda <gunnarholwerda@gmail.com>
2018-11-14 20:22:23 -0800

    Add custom curveball theme, favicon

--- src/app/teleprompter/teleprompter.component.ts
+++ src/app/teleprompter/teleprompter.component.ts
@@ -1,17 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CurrentQuizzes } from '../services/current-quizzes.service';
 import { QuizService } from '../services/quiz.service';
 import { FullQuizResponse } from '../models/quizzes';
 import { RealtimeService } from '../services/realtime.service';
 import { QuestionResults } from '../models/question-results';
 import { IQuestionResponse } from '../models/question';
+import { Subscription } from 'rxjs/internal/Subscription';
 
 @Component({
   selector: 'cb-teleprompter',
   templateUrl: './teleprompter.component.html',
   styleUrls: ['./teleprompter.component.css']
 })
-export class TeleprompterComponent implements OnInit {
+export class TeleprompterComponent implements OnInit, OnDestroy {
+  quizzesSubscription: Subscription;
   quiz: FullQuizResponse;
   questions: Array<IQuestionResponse> = [];
   numConnected: number;
@@ -21,13 +23,19 @@ export class TeleprompterComponent implements OnInit {
   constructor(private currentQuizzes: CurrentQuizzes, private quizService: QuizService, private realTime: RealtimeService) { }
 
   ngOnInit() {
-    this.currentQuizzes.quizStart.subscribe(async (quiz) => {
-      console.log('quizstart', quiz);
+    this.quizzesSubscription = this.currentQuizzes.quizStart.subscribe(async (quiz) => {
       this.quiz = (await this.quizService.getQuiz(quiz.quizId)).quiz;
       this.questions = this.quiz.questions;
       this.pushQuestion();
       this.socket = await this.realTime.connectToQuiz(this.quiz.quizId);
     });
+  }
+
+  ngOnDestroy() {
+    if (this.socket) {
+      this.socket.close();
+    }
+    this.quizzesSubscription.unsubscribe();
   }
 
   set socket(socket: SocketIOClient.Socket) {
